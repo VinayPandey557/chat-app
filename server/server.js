675029d1bbef3bf959987972d477f401a9fff309
Vinay Pandey <vinaypandey46158@gmail.com>
2025-11-20 10:01:13 +0530

    Updatedcode

--- server/server.js
+++ server/server.js
@@ -57,11 +57,12 @@ app.use("/api/messages", messageRouter)
 
 await connectDB();
 
+
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
-
-
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+}
 
-
+export default server;
 
 
